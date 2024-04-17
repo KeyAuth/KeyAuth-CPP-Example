67a2297ee6f6dc1b1d9b4bae91a03e0f66d393ce
Shxde <112267394+Shxde1@users.noreply.github.com>
2024-04-17 18:08:48 -0400

    Add files via upload

---
+++ x64/auth.hpp
@@ -0,0 +1,88 @@
+#include <Windows.h>
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+struct channel_struct
+{
+	std::string author;
+	std::string message;
+	std::string timestamp;
+};
+
+namespace KeyAuth {
+	class api {
+	public:
+
+		std::string name, ownerid, secret, version, url, path;
+
+		api(std::string name, std::string ownerid, std::string secret, std::string version, std::string url, std::string path) : name(name), ownerid(ownerid), secret(secret), version(version), url(url), path(path) {}
+
+		void ban(std::string reason = "");
+		void init();
+		void check();
+		void log(std::string msg);
+		void license(std::string key);
+		std::string var(std::string varid);
+		std::string webhook(std::string id, std::string params, std::string body = "", std::string contenttype = "");
+		void setvar(std::string var, std::string vardata);
+		std::string getvar(std::string var);
+		bool checkblack();
+		void web_login();
+		void button(std::string value);
+		void upgrade(std::string username, std::string key);
+		void login(std::string username, std::string password);
+		std::vector<unsigned char> download(std::string fileid);
+		void regstr(std::string username, std::string password, std::string key, std::string email = "");
+		void chatget(std::string channel);
+		bool chatsend(std::string message, std::string channel);
+		void changeUsername(std::string newusername);
+		std::string fetchonline();
+		void fetchstats();
+		void forgot(std::string username, std::string email);
+
+		class subscriptions_class {
+		public:
+			std::string name;
+			std::string expiry;
+		};
+
+		class userdata {
+		public:
+
+			// user data
+			std::string username;
+			std::string ip;
+			std::string hwid;
+			std::string createdate;
+			std::string lastlogin;
+
+			std::vector<subscriptions_class> subscriptions;
+		};
+
+		class appdata {
+		public:
+			// app data
+			std::string numUsers;
+			std::string numOnlineUsers;
+			std::string numKeys;
+			std::string version;
+			std::string customerPanelLink;
+		};
+
+		class responsedata {
+		public:
+			// response data
+			std::vector<channel_struct> channeldata;
+			bool success{};
+			std::string message;
+		};
+
+		userdata user_data;
+		appdata app_data;
+		responsedata response;
+
+	private:
+		std::string sessionid, enckey;
+	};
+}
---
+++ x64/main.cpp
@@ -0,0 +1,174 @@
+#include <Windows.h>
+#include "auth.hpp"
+#include <string>
+#include "utils.hpp"
+#include "skStr.h"
+std::string tm_to_readable_time(tm ctx);
+static std::time_t string_to_timet(std::string timestamp);
+static std::tm timet_to_tm(time_t timestamp);
+const std::string compilation_date = (std::string)skCrypt(__DATE__);
+const std::string compilation_time = (std::string)skCrypt(__TIME__);
+
+using namespace KeyAuth;
+
+std::string name = skCrypt("name").decrypt();
+std::string ownerid = skCrypt("ownerid").decrypt();
+std::string secret = skCrypt("secret").decrypt();
+std::string version = skCrypt("1.0").decrypt();
+std::string url = skCrypt("https://keyauth.win/api/1.2/").decrypt(); // change if you're self-hosting
+std::string path = skCrypt("path (optional)").decrypt();
+
+api KeyAuthApp(name, ownerid, secret, version, url, path);
+
+int main()
+{
+    // Freeing memory to prevent memory leak or memory scraping
+     name.clear(); ownerid.clear(); secret.clear(); version.clear(); url.clear();
+    std::string consoleTitle = skCrypt("Loader - Built at:  ").decrypt() + compilation_date + " " + compilation_time;
+    SetConsoleTitleA(consoleTitle.c_str());
+    std::cout << skCrypt("\n\n Connecting..");
+    KeyAuthApp.init();
+    if (!KeyAuthApp.response.success)
+    {
+        std::cout << skCrypt("\n Status: ") << KeyAuthApp.response.message;
+        Sleep(1500);
+        exit(1);
+    }
+
+    if (std::filesystem::exists("test.json")) //change test.txt to the path of your file :smile:
+    {
+        if (!CheckIfJsonKeyExists("test.json", "username"))
+        {
+            std::string key = ReadFromJson("test.json", "license");
+            KeyAuthApp.license(key);
+            if (!KeyAuthApp.response.success)
+            {
+                std::remove("test.json");
+                std::cout << skCrypt("\n Status: ") << KeyAuthApp.response.message;
+                Sleep(1500);
+                exit(1);
+            }
+            std::cout << skCrypt("\n\n Successfully Automatically Logged In\n");
+        }
+        else
+        {
+            std::string username = ReadFromJson("test.json", "username");
+            std::string password = ReadFromJson("test.json", "password");
+            KeyAuthApp.login(username, password);
+            if (!KeyAuthApp.response.success)
+            {
+                std::remove("test.json");
+                std::cout << skCrypt("\n Status: ") << KeyAuthApp.response.message;
+                Sleep(1500);
+                exit(1);
+            }
+            std::cout << skCrypt("\n\n Successfully Automatically Logged In\n");
+        }
+    }
+    else
+    {
+        std::cout << skCrypt("\n\n [1] Login\n [2] Register\n [3] Upgrade\n [4] License key only\n\n Choose option: ");
+
+        int option;
+        std::string username;
+        std::string password;
+        std::string key;
+
+        std::cin >> option;
+        switch (option)
+        {
+        case 1:
+            std::cout << skCrypt("\n\n Enter username: ");
+            std::cin >> username;
+            std::cout << skCrypt("\n Enter password: ");
+            std::cin >> password;
+            KeyAuthApp.login(username, password);
+            break;
+        case 2:
+            std::cout << skCrypt("\n\n Enter username: ");
+            std::cin >> username;
+            std::cout << skCrypt("\n Enter password: ");
+            std::cin >> password;
+            std::cout << skCrypt("\n Enter license: ");
+            std::cin >> key;
+            KeyAuthApp.regstr(username, password, key);
+            break;
+        case 3:
+            std::cout << skCrypt("\n\n Enter username: ");
+            std::cin >> username;
+            std::cout << skCrypt("\n Enter license: ");
+            std::cin >> key;
+            KeyAuthApp.upgrade(username, key);
+            break;
+        case 4:
+            std::cout << skCrypt("\n Enter license: ");
+            std::cin >> key;
+            KeyAuthApp.license(key);
+            break;
+        default:
+            std::cout << skCrypt("\n\n Status: Failure: Invalid Selection");
+            Sleep(3000);
+            exit(1);
+        }
+
+        if (!KeyAuthApp.response.success)
+        {
+            std::cout << skCrypt("\n Status: ") << KeyAuthApp.response.message;
+            Sleep(1500);
+            exit(1);
+        }
+        if (username.empty() || password.empty())
+        {
+            WriteToJson("test.json", "license", key, false, "", "");
+            std::cout << skCrypt("Successfully Created File For Auto Login");
+        }
+        else
+        {
+            WriteToJson("test.json", "username", username, true, "password", password);
+            std::cout << skCrypt("Successfully Created File For Auto Login");
+        }
+
+
+    }
+
+    std::cout << skCrypt("\n User data:");
+    std::cout << skCrypt("\n Username: ") << KeyAuthApp.user_data.username;
+    std::cout << skCrypt("\n IP address: ") << KeyAuthApp.user_data.ip;
+    std::cout << skCrypt("\n Hardware-Id: ") << KeyAuthApp.user_data.hwid;
+    std::cout << skCrypt("\n Create date: ") << tm_to_readable_time(timet_to_tm(string_to_timet(KeyAuthApp.user_data.createdate)));
+    std::cout << skCrypt("\n Last login: ") << tm_to_readable_time(timet_to_tm(string_to_timet(KeyAuthApp.user_data.lastlogin)));
+    std::cout << skCrypt("\n Subscription(s): ");
+
+    for (int i = 0; i < KeyAuthApp.user_data.subscriptions.size(); i++) {
+        auto sub = KeyAuthApp.user_data.subscriptions.at(i);
+        std::cout << skCrypt("\n name: ") << sub.name;
+        std::cout << skCrypt(" : expiry: ") << tm_to_readable_time(timet_to_tm(string_to_timet(sub.expiry)));
+    }
+
+    std::cout << skCrypt("\n\n Closing in five seconds...");
+    Sleep(5000);
+
+    return 0;
+}
+
+std::string tm_to_readable_time(tm ctx) {
+    char buffer[80];
+
+    strftime(buffer, sizeof(buffer), "%a %m/%d/%y %H:%M:%S %Z", &ctx);
+
+    return std::string(buffer);
+}
+
+static std::time_t string_to_timet(std::string timestamp) {
+    auto cv = strtol(timestamp.c_str(), NULL, 10); // long
+
+    return (time_t)cv;
+}
+
+static std::tm timet_to_tm(time_t timestamp) {
+    std::tm context;
+
+    localtime_s(&context, &timestamp);
+
+    return context;
+}
