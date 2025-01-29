da2ccb9ae8a9547853d64b11587239325cdba097
Shxde <112267394+Shxde1@users.noreply.github.com>
2025-01-28 19:44:21 -0500

    Update auth.hpp

--- x64/auth.hpp
+++ x64/auth.hpp
@@ -17,7 +17,7 @@ namespace KeyAuth {
 		std::string name, ownerid, version, url, path;
 		static bool debug;
 		
-		api(std::string name, std::string ownerid, std::string version, std::string url, std::string path, bool debug = false) {
+		api(std::string name, std::string ownerid, std::string version, std::string url, std::string path, bool debugParameter = false) {
 			setDebug(debugParameter);
 		}
 
